22b2fe018b6251a5a3c1438011b156890a2d017c
anton.minko <anton.minko@grammarly.com>
2020-08-30 13:37:47 +0300

    Add problem description

--- Solutions.FSharp/Easy/CommonPrefix.fs
+++ Solutions.FSharp/Easy/CommonPrefix.fs
@@ -1,3 +1,28 @@
+    (*
+    Link: https://leetcode.com/problems/longest-common-prefix/
+    
+    Difficulty: Easy
+
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+Example 1:
+
+Input: ["flower","flow","flight"]
+Output: "fl"
+
+Example 2:
+
+Input: ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+
+Note:
+
+All given inputs are in lowercase letters a-z.
+
+    *)
 module Solutions.FSharp.Easy.CommonPrefix
 
     let rec getCommonPrefix (str1 :List<char>, str2: List<char>) =
