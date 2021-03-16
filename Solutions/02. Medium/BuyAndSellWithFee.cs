b2da8196340cd52ce728cddbd7af5000e96878a4
anton.minko <anton.minko@grammarly.com>
2021-03-16 10:39:33 +0200

    714. Best Time to Buy and Sell Stock with Transaction Fee

---
+++ Solutions/02. Medium/BuyAndSellWithFee.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Solutions
+{
+    /*
+    714. Best Time to Buy and Sell Stock with Transaction Fee
+
+    Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
+    
+    Difficulty: Medium
+
+You are given an array prices where prices[i] is the price of a given stock on the ith day, and an integer fee representing a transaction fee.
+Find the maximum profit you can achieve. You may complete as many transactions as you like, but you need to pay the transaction fee for each transaction.
+Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+Example 1:
+Input: prices = [1,3,2,8,4,9], fee = 2
+Output: 8
+Explanation: The maximum profit can be achieved by:
+- Buying at prices[0] = 1
+- Selling at prices[3] = 8
+- Buying at prices[4] = 4
+- Selling at prices[5] = 9
+The total profit is ((8 - 1) - 2) + ((9 - 4) - 2) = 8.
+
+Example 2:
+Input: prices = [1,3,7,5,10,3], fee = 3
+Output: 6
+
+ 
+
+Constraints:
+
+    1 < prices.length <= 5 * 10^4
+    0 < prices[i], fee < 5 * 10^4
+
+    Complexity:
+        O(n) time complexity
+        O(1) space complexity
+    */
+    public class BuyAndSellWithFee
+    {
+        public int MaxProfit(int[] prices, int fee)
+        {
+            if (prices.Length < 2) return 0;
+
+            int profit = 0;
+            int buyPrice = prices[0];
+            int sellPrice = -1;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] > sellPrice && prices[i] > buyPrice + fee)
+                {
+                    sellPrice = prices[i];
+                }
+                else if (prices[i] < sellPrice - fee)
+                {
+                    profit += sellPrice - buyPrice - fee;
+                    buyPrice = prices[i];
+                    sellPrice = -1;
+                }
+                else if (prices[i] < buyPrice)
+                {
+                    buyPrice = prices[i];
+                    sellPrice = -1;
+                }
+            }
+
+            if (sellPrice - buyPrice - fee > 0)
+            {
+                profit += sellPrice - buyPrice - fee;
+            }
+
+            return profit;
+        }
+    }
+
+}
---
+++ Tests/02. Medium/BuyAndSellWithFeeTests.cs
@@ -0,0 +1,31 @@
+﻿using Solutions;
+using Xunit;
+using Xunit.Abstractions;
+using System.Text;
+using System.Diagnostics;
+using FluentAssertions;
+
+namespace Tests
+{
+    public class BuyAndSellWithFeeTests : BaseTests
+    {
+        public BuyAndSellWithFeeTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Theory]
+        [InlineData(1, new int[] { 5 }, 1, 0)]
+        [InlineData(2, new int[] { 1, 3 }, 2, 0)]
+        [InlineData(3, new int[] { 1, 4 }, 2, 1)]
+        [InlineData(4, new int[] { 4, 1 }, 1, 0)]
+        [InlineData(5, new int[] { 1, 3, 7, 5, 10, 3 }, 3, 6)]
+        [InlineData(6, new int[] { 1, 3, 2, 8, 4, 9 }, 2, 8)]
+        public void DataDrivenTest(int testId, int[] prices, int fee, int expected)
+        {
+            var sut = new BuyAndSellWithFee();
+            var actual = MeasureAndExecute(testId, () => sut.MaxProfit(prices, fee));
+
+            actual.Should().Be(expected);
+        }
+    }
+}
