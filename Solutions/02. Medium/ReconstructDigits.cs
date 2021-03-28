9bfee7fdaec65a022dc86debb9e14a70ac5240f0
anton.minko <anton.minko@grammarly.com>
2021-03-28 21:22:38 +0300

    423. Reconstruct Original Digits from English

---
+++ Solutions/02. Medium/ReconstructDigits.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Solutions
+{
+    /*
+    423. Reconstruct Original Digits from English
+
+    Link: https://leetcode.com/problems/reconstruct-original-digits-from-english/
+    
+    Difficulty: Medium
+
+Given a non-empty string containing an out-of-order English representation of digits 0-9, output the digits in ascending order.
+
+Note:
+    Input contains only lowercase English letters.
+    Input is guaranteed to be valid and can be transformed to its original digits. That means invalid inputs such as "abc" or "zerone" are not permitted.
+    Input length is less than 50,000.
+
+Example 1:
+Input: "owoztneoer"
+Output: "012"
+
+Example 2:
+Input: "fviefuro"
+Output: "45"
+
+    Complexity:
+        O(n) time complexity
+        O(n) space complexity
+    */
+    public class ReconstructDigits
+    {
+        public string OriginalDigits(string s)
+        {
+            var used = new bool[s.Length];
+            var expected = new Dictionary<char, int>();
+            var sb = new StringBuilder();
+
+            int CountChars(char charToCount)
+            {
+                int count = 0;
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (!used[i] && s[i] == charToCount)
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+
+            void AddToExpected(int digit, int count)
+            {
+                if (count == 0) return;
+
+                expected.Clear();
+
+                switch (digit)
+                {
+                    case 0:
+                        expected.Add('z', count);
+                        expected.Add('e', count);
+                        expected.Add('r', count);
+                        expected.Add('o', count);
+                        break;
+                    case 1:
+                        expected.Add('o', count);
+                        expected.Add('n', count);
+                        expected.Add('t', count);
+                        break;
+                    case 2:
+                        expected.Add('t', count);
+                        expected.Add('w', count);
+                        expected.Add('o', count);
+                        break;
+                    case 3:
+                        expected.Add('t', count);
+                        expected.Add('h', count);
+                        expected.Add('r', count);
+                        expected.Add('e', count * 2);
+                        break;
+                    case 4:
+                        expected.Add('f', count);
+                        expected.Add('o', count);
+                        expected.Add('u', count);
+                        expected.Add('r', count);
+                        break;
+                    case 5:
+                        expected.Add('f', count);
+                        expected.Add('i', count);
+                        expected.Add('v', count);
+                        expected.Add('e', count);
+                        break;
+                    case 6:
+                        expected.Add('s', count);
+                        expected.Add('i', count);
+                        expected.Add('x', count);
+                        break;
+                    case 7:
+                        expected.Add('s', count);
+                        expected.Add('e', count * 2);
+                        expected.Add('v', count);
+                        expected.Add('n', count);
+                        break;
+                    case 8:
+                        expected.Add('e', count);
+                        expected.Add('i', count);
+                        expected.Add('g', count);
+                        expected.Add('h', count);
+                        expected.Add('t', count);
+                        break;
+                    case 9:
+                    default:
+                        expected.Add('n', count * 2);
+                        expected.Add('i', count);
+                        expected.Add('e', count);
+                        break;
+                }
+            }
+
+            void MarkAsUsed(int digit, int count)
+            {
+                AddToExpected(digit, count);
+
+                for (int i = 0; i < s.Length; i++)
+                {
+                    char c = s[i];
+                    if (!used[i] && expected.ContainsKey(c))
+                    {
+                        used[i] = true;
+
+                        if (expected[c] == 1)
+                        {
+                            expected.Remove(c);
+                        }
+                        else
+                        {
+                            expected[c] -= 1;
+                        }
+                    }
+                }
+            }
+
+            var result = new int[10];
+            result[0] = CountChars('z');
+            MarkAsUsed(0, result[0]);
+
+            result[2] = CountChars('w');
+            MarkAsUsed(2, result[2]);
+
+            result[4] = CountChars('u');
+            MarkAsUsed(4, result[4]);
+
+            result[6] = CountChars('x');
+            MarkAsUsed(6, result[6]);
+
+            result[8] = CountChars('g');
+            MarkAsUsed(8, result[8]);
+
+            result[3] = CountChars('r');
+            MarkAsUsed(3, result[3]);
+
+            result[5] = CountChars('f');
+            MarkAsUsed(5, result[5]);
+
+            result[7] = CountChars('s');
+            MarkAsUsed(7, result[7]);
+
+            result[1] = CountChars('o');
+            MarkAsUsed(1, result[1]);
+
+            result[9] = CountChars('i');
+            MarkAsUsed(9, result[9]);
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < result[i]; j++)
+                {
+                    sb.Append(i);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+
+}
---
+++ Tests/02. Medium/ReconstructDigitsTests.cs
@@ -0,0 +1,32 @@
+﻿using Solutions;
+using Xunit;
+using Xunit.Abstractions;
+using System.Text;
+using System.Diagnostics;
+using FluentAssertions;
+
+namespace Tests
+{
+    public class ReconstructDigitsTests : BaseTests
+    {
+        public ReconstructDigitsTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Theory]
+        [InlineData(1, "owoztneoer", "012")]
+        [InlineData(2, "nnie", "9")]
+        [InlineData(3, "owoztneoernnneeeooo", "011112")]
+        [InlineData(4, "roze", "0")]
+        [InlineData(5, "reteh", "3")]
+        [InlineData(6, "foveurfixneevssigneeiinht", "456789")]
+        [InlineData(7, "foveurfixneevssigneeiinhtowoztneoerfoveurfixneevssigneeiinht", "012445566778899")]
+        public void DataDrivenTest(int testId, string s, string expected)
+        {
+            var sut = new ReconstructDigits();
+            var actual = MeasureAndExecute(testId, () => sut.OriginalDigits(s));
+
+            actual.Should().Be(expected);
+        }
+    }
+}
