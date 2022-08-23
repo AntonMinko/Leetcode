b89e51797c9ba27b6906172f586e6a7d375dbe52
AntonMinko <minko.anton@gmail.com>
2022-08-23 17:26:43 +0200

    5. Longest Palindromic Substring

---
+++ Solutions/02. Medium/LongestPalindromicSubstring.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Solutions
+{
+    /*
+    5. Longest Palindromic Substring
+
+    Link: https://leetcode.com/problems/longest-palindromic-substring/
+    
+    Difficulty: Medium
+
+Given a string s, find the longest palindromic substring in s. You may assume that 
+the maximum length of s is 1000.
+
+Example 1:
+Input: "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+
+Example 2:
+Input: "cbbd"
+Output: "bb"
+    */
+    public class LongestPalindromicSubstring
+    {
+        public string LongestPalindrome(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return s;
+
+            int maxLength = 0;
+            int start=0;
+            int l = 0;
+            int r = s.Length - 1;
+
+            while (s.Length - l >= maxLength)
+            {
+                r = s.Length - 1;
+                while (r-l >= maxLength)
+                {
+                    if (IsPalindrome(s, l, r))
+                    {
+                        maxLength = r-l+1;
+                        start = l;
+                        break;
+                    }
+                    r--;
+                }
+                l++;
+            }
+            return s.Substring(start, maxLength);
+        }
+
+        private bool IsPalindrome(string s, int l, int r)
+        {
+            while (l <= r)
+            {
+                if (s[l] != s[r]) return false;
+
+                l++;
+                r--;
+            }
+            return true;
+        }
+    }
+
+}
---
+++ Tests/02. Medium/LongestPalindromicSubstringTests.cs
@@ -0,0 +1,36 @@
+﻿using Solutions;
+using Xunit;
+using Xunit.Abstractions;
+using System.Text;
+using System.Diagnostics;
+using FluentAssertions;
+
+namespace Tests
+{
+    public class LongestPalindromicSubstringTests
+    {
+        private readonly ITestOutputHelper output;
+
+        public LongestPalindromicSubstringTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Theory]
+        [InlineData("babad", "bab")]
+        [InlineData("cbbd", "bb")]
+        [InlineData("babab", "babab")]
+        [InlineData("besad", "b")]
+        [InlineData("babababa", "bababab")]
+        [InlineData("sabababa", "abababa")]
+        [InlineData("", "")]
+        [InlineData("b", "b")]
+        [InlineData("acbdaaebca", "aa")]
+        public void DataDrivenTest(string s, string expected)
+        {
+            var sut = new LongestPalindromicSubstring();
+            string actual = sut.LongestPalindrome(s);
+            actual.Should().Be(expected, $"s={s}, expected={expected}, actual={actual}");
+        }
+    }
+}
